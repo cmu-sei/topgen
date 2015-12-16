e901384bfa2901c2fc9875c8ea953cd3efd18432
Gabriel Somlo <glsomlo@cert.org>
2015-12-16 13:04:24 -0500

    First packaged releaseable version of TopGen
    The process of scraping sites with wget, creating the nginx vhost
    hierarchy, signing all of them with a generated CA cert, etc. has
    been codified into a flexible script named topgen-scrape.sh
    
    The process of generating a multi-view bind9 DNS configuration based
    on all collected nginx vhosts has been codified into another script
    named topgen-mkdns.sh
    
    Systemd unit files to automatically generate dns configuration, set
    up loopbacks, and fire up nginx and bind9 are included under ./systemd
    
    Also included are example files for kickstarting a minimal Fedora 22
    machine with TopGen installed (greybox_f22.ks), a spec file to build
    a topgen rpm package, and example topology files to stand up TopGen
    inside a container on the CORE network emulator.
    
    Signed-off-by: Gabriel Somlo <glsomlo@cert.org>

---
+++ contrib/topgen.spec
@@ -0,0 +1,73 @@
+Name: topgen
+Version: 0.0.1
+Release: 1%{?dist}
+Summary: TopGen Exercise Internet Simulator
+License: BSD
+Url: http://cert.org
+Source0: http://download.cert.org/%{name}-%{version}.tar.xz
+Requires(post): systemd-units
+Requires(preun): systemd-units
+Requires(postun): systemd-units
+Requires: coreutils, gawk, grep, sed, openssl, iproute
+Requires: bind, nginx, wget >= 1.17 
+BuildRequires: systemd-units
+BuildArch: noarch
+
+%description
+TopGen provides a simulation of the Internet (Web, DNS, etc.) for
+cybersecurity exercise environments.
+
+%prep
+%setup -q
+
+%build
+echo "nothing to build"
+
+%install
+install -d %{buildroot}/%{_unitdir}
+install -d %{buildroot}/%{_sbindir}
+install -d %{buildroot}/%{_sysconfdir}/nginx/conf.d
+install -d %{buildroot}/%{_sysconfdir}/%{name}
+install -d %{buildroot}/%{_localstatedir}/lib/%{name}/etc
+install -d %{buildroot}/%{_localstatedir}/lib/%{name}/vhosts
+install -d %{buildroot}/%{_localstatedir}/lib/%{name}/certs
+install -d %{buildroot}/%{_localstatedir}/lib/%{name}/named
+%{__ln_s} %{_localstatedir}/lib/%{name}/etc/nginx.conf \
+          %{buildroot}%{_sysconfdir}/nginx/conf.d/topgen.conf
+install -m 0644 -t %{buildroot}/%{_unitdir} systemd/*
+install -m 0755 -t %{buildroot}/%{_sbindir} sbin/*
+install -m 0644 -t %{buildroot}/%{_sysconfdir}/%{name} etc/*
+
+%post
+%systemd_post topgen-named.service topgen-nginx.service
+
+%preun
+%systemd_preun topgen-named.service topgen-nginx.service
+
+%postun
+%systemd_postun_with_restart topgen-named.service topgen-nginx.service
+
+%files
+%defattr(-,root,root,-)
+# miscellaneous doc files and samples:
+%doc README LICENSE* TODO contrib
+# systemd unit files:
+%{_unitdir}/topgen*
+# nginx symlink to topgen-specific configuration:
+%{_sysconfdir}/nginx/conf.d/topgen.conf
+# /etc/topgen directory and config files:
+%dir %{_sysconfdir}/%{name}
+%config(noreplace) %{_sysconfdir}/%{name}/scrape_sites.txt
+%config(noreplace) %{_sysconfdir}/%{name}/delegations.dns
+# topgen scripts:
+%{_sbindir}/topgen*
+# (initially empty) directory structure for storing topgen data:
+%dir %{_localstatedir}/lib/%{name}
+%dir %{_localstatedir}/lib/%{name}/etc
+%dir %{_localstatedir}/lib/%{name}/vhosts
+%dir %{_localstatedir}/lib/%{name}/certs
+%dir %{_localstatedir}/lib/%{name}/named
+
+%changelog
+* Tue Dec 15 2015 Gabriel Somlo <glsomlo at cert.org> 0.0.0.1-1
+- initial fedora package
---
+++ sbin/topgen-scrape.sh
@@ -0,0 +1,313 @@
+#!/bin/bash
+
+# Scrape and curate a corpus of Web sites for TopGen
+# (glsomlo@cert.org, December 2015)
+
+# input: original list of sites to scrape:
+TOPGEN_ORIG="/etc/topgen/scrape_sites.txt"
+
+# topgen corpus directory structure:
+TOPGEN_VARLIB='/var/lib/topgen'
+
+# if "yes", do not print out warnings:
+let VERBOSE=0
+
+# recursive scrape depth:
+let DEPTH=1
+
+###########################################################################
+####    NO FURTHER USER-SERVICEABLE PARTS BEYOND THIS POINT !!!!!!!    ####
+###########################################################################
+
+USAGE_BLURB="
+Usage: $0 [-s <site_list>] [-t <target_directory>]
+
+The optional command line arguments are:
+
+    -s <site_list>        file containing space or newline separated sites
+                          to be scraped for static content; lines beginning
+                          with '#' are ignored;
+                          (default: $TOPGEN_ORIG).
+
+    -t <target_directory> directory where all results (scraped content, list
+                          of vhosts, certificates, configuration files, etc.
+                          are stored;
+                          (default: $TOPGEN_VARLIB).
+
+    -v                    increase verbosity level (may be used multiple times);
+                          (default: $VERBOSE).
+
+Recursively scrape, clean, curate a given list of Web sites. Additionally,
+issue certificates signed with a self-signed TopGen CA (which is in turn
+also generated, if necessary). Generate a drop-in config file for the nginx
+HTTP server, and a hosts file containing <ip_addr fqdn> entries for each
+scraped vhost.
+" # end usage blurb
+
+# process command line options:
+OPTIND=1
+while getopts "s:t:vh?" OPT; do
+  case "$OPT" in
+  s)
+    TOPGEN_ORIG=$OPTARG
+    ;;
+  t)
+    TOPGEN_VARLIB=$OPTARG
+    ;;
+  v)
+    let VERBOSE++
+    ;;
+  *)
+    echo "$USAGE_BLURB"
+    exit 0
+    ;;
+  esac
+done
+
+# we should be left with NO FURTHER arguments on the command line:
+shift $((OPTIND-1))
+[ -z "$@" ] || {
+  echo "
+ERROR: invalid argument: $@
+
+$USAGE_BLURB
+"
+  exit 1
+}
+
+# once $TOPGEN_VARLIB is set, these are its relevant subdirectories:
+TOPGEN_VARETC="$TOPGEN_VARLIB/etc"
+TOPGEN_CERTS="$TOPGEN_VARLIB/certs"
+TOPGEN_VHOSTS="$TOPGEN_VARLIB/vhosts"
+# topgen.info vhost directory:
+TOPGEN_SITE="$TOPGEN_VHOSTS/topgen.info"
+
+# assert existence of required input files and target folders:
+[ -s "$TOPGEN_ORIG" -a \
+  -d "$TOPGEN_VHOSTS" -a -d "$TOPGEN_VARETC" -a -d "$TOPGEN_CERTS" ] || {
+  echo "
+ERROR: file \"$TOPGEN_ORIG\" and
+       folders \"$TOPGEN_VHOSTS\",
+               \"$TOPGEN_VARETC\", and
+               \"$TOPGEN_CERTS\" MUST exist
+       before running this command!
+
+$USAGE_BLURB
+"
+  exit 1
+}
+
+# assert target folder is empty:
+[ -n "$(ls -A $TOPGEN_VHOSTS)" ] && {
+  echo "
+ERROR: folder \"$TOPGEN_VHOSTS\" MUST be empty
+       before running this command!
+
+$USAGE_BLURB
+"
+  exit 1
+}
+
+# default wget options:
+WGET_OPTS='-prEHN --convert-file-only --no-check-certificate -e robots=off --random-wait -t 2'
+# make wget quiet, unless verbosity >= 2:
+((VERBOSE>=2)) || WGET_OPTS="$WGET_OPTS -q"
+
+# wget URL list:
+WGET_URLS=$(grep -v '^#' "$TOPGEN_ORIG" | \
+            tr '[:space:]' ' ' | sed -e 's/[[:space:]]*$/\n/')
+[ -n "$WGET_URLS" ] || {
+  echo "
+ERROR: At least one uncommented URL must be specified
+       in \"$TOPGEN_ORIG\" !
+"
+  exit 1
+}
+
+# recursively scrape all sites listed in the $TOPGEN_ORIG file:
+wget $WGET_OPTS -U "Mozilla/5.0 (X11)" -l $DEPTH -P $TOPGEN_VHOSTS $WGET_URLS
+
+# wget will likely return some kind of error on a large recursive scrape;
+# we are only really interested if it actually segfaults:
+[ $? -eq 139 ] && {
+  echo "
+ERROR: recursive wget scrape encountered a segfault!
+"
+  exit 1
+}
+
+# cleanup; remove IP-only vhosts, and vhosts ending with a ":<port-number>":
+shopt -s extglob
+rm -rf $TOPGEN_VHOSTS/+([[:digit:].]) $TOPGEN_VHOSTS/*:*
+
+# "curate" remaining vhosts; handle [www.]example.org/index.html issue:
+for VH in $TOPGEN_VHOSTS/*; do
+  VB=${VH##*/} # basename
+  NUM=$(ls $VH | wc -l)
+  # if example.org has only one file named "index.html", AND if
+  # www.example.org exists and DOESN'T contain "index.html", apply the fix:
+  if [ $NUM -eq 1 -a -f "$VH/index.html" -a -d "$TOPGEN_VHOSTS/www.$VB" \
+       -a ! -f "$TOPGEN_VHOSTS/www.$VB/index.html" ]; then
+    cp "$VH/index.html" "$TOPGEN_VHOSTS/www.$VB/"
+    ((VERBOSE)) && echo "curate: $VH/index.html -> $TOPGEN_VHOSTS/www.$VB/"
+  fi
+done
+
+# topgen.info is reserved, be sure to create it completely from scratch:
+rm -rf "$TOPGEN_SITE"
+mkdir "$TOPGEN_SITE"
+
+# intersection of scraped vhosts in $TOPGEN_VHOSTS against $TOPGEN_ORIG
+comm -12 <(ls -1 $TOPGEN_VHOSTS) \
+         <(grep -v '^#' "$TOPGEN_ORIG" | tr '[:space:]' '\n' | \
+           sed -e 's|^.*//||; s|/.*$||' | sort -u) \
+  > "$TOPGEN_SITE/orig_vhosts.txt"
+
+# generate topgen.info landing page (index.html):
+{
+  cat <<- "EOT"
+	<html>
+	  <head>
+	    <title>Welcome to TopGen.info !</title>
+	  </head>
+	  <body> <div style="text-align: justify; width: 500pt">
+	    <h2>Welcome to TopGen.info !</h2>
+	    This is a simulation of the World Wide Web. View this site in either
+	    <ul>
+	    <li> Cleartext: <a href="http://topgen.info">http://topgen.info</a>
+	    <li> HTTPS: <a href="https://topgen.info">https://topgen.info</a>;
+	      <ul>
+	      <li> Your browser requires the
+	           <a href="topgen_ca.cer">TopGen CA Certificate</a>
+	           to avoid certificate warnings! All simulated Web sites are
+	           using certificates issued and signed by this CA!
+	      </ul>
+	    </ul>
+	    Below is a list of Web sites mirrored for this simulation:
+	    <ul>
+	EOT
+  for VH in $(cat "$TOPGEN_SITE/orig_vhosts.txt"); do
+    echo "    <li><a href=\"//$VH\">$VH</a>"
+  done
+  cat <<- "EOT"
+	    </ul>
+	  </body>
+	</html>
+	EOT
+} > "$TOPGEN_SITE/index.html"
+
+# NOTE: hand-crafted vhosts should manually be added to $TOPGEN_VHOSTS here!
+
+# generate TopGen CA (unless already provided):
+[ -s "$TOPGEN_VARETC/topgen_ca.key" -a -s "$TOPGEN_VARETC/topgen_ca.cer" ] ||
+  openssl req -newkey rsa:2048 -nodes -keyout "$TOPGEN_VARETC/topgen_ca.key" \
+              -days 7300 -x509 -out "$TOPGEN_VARETC/topgen_ca.cer" \
+              -subj '/C=US/ST=PA/L=Pgh/O=CMU/OU=CERT/CN=topgen_ca' \
+              2>/dev/null
+# ... and place a copy in $TOPGEN_SITE:
+cp "$TOPGEN_VARETC/topgen_ca.cer" "$TOPGEN_SITE"
+
+# generate TopGen vhost key (unless already provided):
+[ -s "$TOPGEN_VARETC/topgen_vh.key" ] ||
+  openssl genrsa -out "$TOPGEN_VARETC/topgen_vh.key" 2048 2>/dev/null
+
+# create CA configuration (file and /tmp directory structure):
+TMP_CA_DIR=$(mktemp -d /tmp/TopGenCA.XXXXXXXXXXXXXX)
+echo "000a" > "$TMP_CA_DIR/serial" # seed the serial file
+touch "$TMP_CA_DIR/index" # empty (but required) index file
+
+# CA configuration file:
+TMP_CA_CONF="[ ca ]
+default_ca = topgen_ca
+
+[ crl_ext ]
+authorityKeyIdentifier=keyid:always
+
+[ topgen_ca ]
+private_key = $TOPGEN_VARETC/topgen_ca.key
+certificate = $TOPGEN_VARETC/topgen_ca.cer
+new_certs_dir = $TMP_CA_DIR
+database = $TMP_CA_DIR/index
+serial = $TMP_CA_DIR/serial
+default_days = 3650
+default_md = sha1
+copy_extensions = copy
+unique_subject = no
+policy = topgen_ca_policy
+x509_extensions = topgen_ca_ext
+
+[ topgen_ca_policy ]
+countryName = supplied
+stateOrProvinceName = supplied
+localityName = supplied
+organizationName = supplied
+organizationalUnitName = supplied
+commonName = supplied
+emailAddress = optional
+
+[ topgen_ca_ext ]
+basicConstraints = CA:false
+nsCertType = server
+nsComment = \"TopGen CA Generated Certificate\"
+subjectKeyIdentifier = hash
+authorityKeyIdentifier = keyid,issuer:always
+"
+
+# per-vhost CSR configuration template:
+# (NOTE: using [alt_names] to work around CN length limit for long hostnames)
+TMP_VH_CONF=$(sed -e '/req_extensions/s/^# //;
+                      /^\[ v3_req \]/a subjectAltName = @alt_names' \
+                  /etc/pki/tls/openssl.cnf)
+
+# start nginx.conf here (vhost entries to be appended from loop below)
+cat > "$TOPGEN_VARETC/nginx.conf" <<- EOT
+	server {
+	  listen 80 default_server;
+	  server_name \$http_host;
+	  root $TOPGEN_VHOSTS/\$http_host;
+	}
+
+	# use a common key for all certificates:
+	ssl_certificate_key $TOPGEN_VARETC/topgen_vh.key;
+
+	# ensure enumerated https server blocks fit into nginx hash table:
+	server_names_hash_bucket_size 256;
+	server_names_hash_max_size 131070;
+EOT
+
+# nuke hosts.nginx (vhost entries to be appended below):
+rm -f "$TOPGEN_VARETC/hosts.nginx"
+
+# process each vhost:
+for VH in $TOPGEN_VHOSTS/*; do
+  VB=${VH##*/} # basename
+
+  # issue certificate (based on csr generated on-the-fly):
+  openssl ca -batch -notext \
+          -config <(echo "$TMP_CA_CONF") -out "$TOPGEN_CERTS/$VB.cer" \
+          -in <(openssl req -new -key "$TOPGEN_VARETC/topgen_vh.key" \
+                        -subj '/C=US/ST=PA/L=Pgh/O=CMU/OU=CERT/CN=topgen_vh' \
+                        -config <(echo "$TMP_VH_CONF"
+                                  echo -e "[alt_names]\nDNS.1 = $VB") \
+                        2>/dev/null) \
+          2>/dev/null
+
+  # append vhost https block to nginx.conf file:
+  cat >> "$TOPGEN_VARETC/nginx.conf" <<- EOT
+
+	server {
+	  listen 443 ssl;
+	  ssl_certificate $TOPGEN_CERTS/$VB.cer;
+	  server_name $VB;
+	  root $VH;
+	}
+	EOT
+
+  # resolve vhost DNS IP address, write to hosts.nginx
+  VHIP=$(getent ahostsv4 $VB | head -1 | cut -d' ' -f1)
+  [ -n "$VHIP" ] || VHIP='0.64.83.76' # made-up IP for unresolvable vhosts
+  echo "$VHIP $VB" >> "$TOPGEN_VARETC/hosts.nginx"
+done
+
+# done with CA directory:
+rm -rf "$TMP_CA_DIR"

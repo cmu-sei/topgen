bf396c41a28b2f0685f415823df483788baad905
Gabriel Somlo <glsomlo@cert.org>
2018-04-20 10:48:50 -0400

    install.sh: fix install binary path (set to /usr/bin)
    Signed-off-by: Gabriel Somlo <glsomlo@cert.org>

--- install.sh
+++ install.sh
@@ -12,19 +12,19 @@
 /bin/echo "SBINDIR      =${SBINDIR:=/sbin};"
 /bin/echo "MANDIR       =${MANDIR:=/usr/share/man};"
 
-/bin/install -d $BUILDROOT/$UNITDIR
-/bin/install -d $BUILDROOT/$SBINDIR
-/bin/install -d $BUILDROOT/$MANDIR/man8
-/bin/install -d $BUILDROOT/$SYSCONFDIR/nginx/conf.d
-/bin/install -d $BUILDROOT/$SYSCONFDIR/$NAME
-/bin/install -d $BUILDROOT/$LOCALSTATEDIR/lib/$NAME/etc/postfix
-/bin/install -d $BUILDROOT/$LOCALSTATEDIR/lib/$NAME/vhosts
-/bin/install -d $BUILDROOT/$LOCALSTATEDIR/lib/$NAME/certs
-/bin/install -d $BUILDROOT/$LOCALSTATEDIR/lib/$NAME/named
-/bin/install -d $BUILDROOT/$LOCALSTATEDIR/lib/$NAME/vmail
+/usr/bin/install -d $BUILDROOT/$UNITDIR
+/usr/bin/install -d $BUILDROOT/$SBINDIR
+/usr/bin/install -d $BUILDROOT/$MANDIR/man8
+/usr/bin/install -d $BUILDROOT/$SYSCONFDIR/nginx/conf.d
+/usr/bin/install -d $BUILDROOT/$SYSCONFDIR/$NAME
+/usr/bin/install -d $BUILDROOT/$LOCALSTATEDIR/lib/$NAME/etc/postfix
+/usr/bin/install -d $BUILDROOT/$LOCALSTATEDIR/lib/$NAME/vhosts
+/usr/bin/install -d $BUILDROOT/$LOCALSTATEDIR/lib/$NAME/certs
+/usr/bin/install -d $BUILDROOT/$LOCALSTATEDIR/lib/$NAME/named
+/usr/bin/install -d $BUILDROOT/$LOCALSTATEDIR/lib/$NAME/vmail
 /bin/ln -s $LOCALSTATEDIR/lib/$NAME/etc/nginx.conf \
            $BUILDROOT/$SYSCONFDIR/nginx/conf.d/topgen.conf
-/bin/install -m 0644 -t $BUILDROOT/$UNITDIR systemd/*
-/bin/install -m 0755 -t $BUILDROOT/$SBINDIR sbin/*
-/bin/install -m 0644 -t $BUILDROOT/$MANDIR/man8 man/*
-/bin/install -m 0644 -t $BUILDROOT/$SYSCONFDIR/$NAME etc/*
+/usr/bin/install -m 0644 -t $BUILDROOT/$UNITDIR systemd/*
+/usr/bin/install -m 0755 -t $BUILDROOT/$SBINDIR sbin/*
+/usr/bin/install -m 0644 -t $BUILDROOT/$MANDIR/man8 man/*
+/usr/bin/install -m 0644 -t $BUILDROOT/$SYSCONFDIR/$NAME etc/*
